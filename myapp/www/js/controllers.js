9b379e7c445e5005e793bfaa29f35a78e27e208c
PaulDabin <dabin.paul@gmail.com>
2016-05-20 10:20:44 +0200

    ajout controller

--- myapp/www/js/controllers.js
+++ myapp/www/js/controllers.js
@@ -2,6 +2,11 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
+    .controller('RandomCtrl', function($scope) {
+
+        getRandomImage(random_images_array, '../img/')
+    })
+
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
